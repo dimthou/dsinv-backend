6e9c8c94952955f59fa2b746e9f9adab630fd65c
Thou <>
2025-09-29 11:52:57 +0700

    feat(setup): initial project scaffolding

---
+++ src/main/java/com/ds/dsinv/DsinvApplication.java
@@ -0,0 +1,13 @@
+package com.ds.dsinv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsinvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsinvApplication.class, args);
+	}
+
+}
